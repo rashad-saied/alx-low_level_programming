383eac0bff0f1527261b55798c54e10cf729b3a5
rashad7774 <rashadtech7@gmail.com>
2023-03-28 00:50:42 +0200

    unction that prints a string, in reverse, followed by a new line.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - the length of a string.
+ * @s : the char pointer
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+/**
  * print_rev - prints a string, in reverse, followed by a new line..
  * @s : the char pointer
  *
