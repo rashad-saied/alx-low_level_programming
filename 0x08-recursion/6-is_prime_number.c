ebe45117874add0163b541c6cfcd391b3acfe23c
rashad7774 <rashadtech7@gmail.com>
2023-04-06 00:42:39 +0200

    function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,6 +2,52 @@
 #include <string.h>
 
 /**
+ * square_root_shadow - calculate the square root using guess numbers
+ * @num: the given number
+ * @min: minimum guess number
+ * @max: maximum guess number
+ *
+ * Return: square root integer
+ */
+
+int square_root_shadow(int num, int min, int max)
+{
+	int long guess;
+	int long guess_square;
+
+	if (min > max)
+		return (-1);
+	guess = (min + max) / 2;
+	guess_square = guess * guess;
+
+	if (guess_square == num)
+	{
+		return (guess);
+	}
+	else if (guess_square < num)
+	{
+		return (square_root_shadow(num, min + 1, num));
+	}
+	else
+	{
+		return (square_root_shadow(num, min, guess - 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - the natural square root of a number.
+ * @n: the given number
+ *
+ * Return: integer of the square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square_root_shadow(n, 1, n));
+}
+
+
+/**
  * prime_number_validation - check if a number is a prime or not
  * @n: the number to be checked
  * @i: the divisor
