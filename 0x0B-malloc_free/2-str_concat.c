18632ab372a5a48faf3691b9297fab21223962a4
rashad7774 <rashadtech7@gmail.com>
2023-04-11 18:16:35 +0200

    a function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,11 +17,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = '';
+		*s1 = '\0';
 	}
 	else if (s2 == NULL)
 	{
-		s2 = '';
+		*s2 = '\0';
 	}
 	len_s1 = strlen(s1);
 	len_s2 = strlen(s2);
