dbb3a0e80bbfbd4d0c92f9836706dae28d1d8558
rashad7774 <rashadtech7@gmail.com>
2023-04-11 15:31:42 +0200

    Initialize new project to manipulate Dynamic memory allocation in C

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,13 +4,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
+	unsigned int i;
 
 	arr = (char *) malloc(sizeof(char) * size);
 	if (size == 0 || arr == NULL)
 	{
 		return (NULL);
 	}
-	*arr = c;
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
 	return (arr);
 }
 
