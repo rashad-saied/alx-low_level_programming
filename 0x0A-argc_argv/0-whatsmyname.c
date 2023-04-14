972066990d95415dfa9c430c74bca98570c2d0b8
rashad7774 <rashadtech7@gmail.com>
2023-04-14 11:11:57 +0200

    Initialize a new project to manipulate command line arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - check the code for ALX School students.
+ * @argc: number of arguments
+ * @argv: pointer to an array of strings representing the arguments
+ *
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 	(void) argc;
