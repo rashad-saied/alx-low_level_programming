8369432f0d49fe8af94114cce90bb33b40467c19
rashad7774 <rashadtech7@gmail.com>
2023-04-19 16:47:02 +0200

    Initialize a new project to manipulate function pointers in C

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a given name
+ * @name: the given name
+ * @f: a pointer to a function that takes a char pointer as an argument
+ *
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+# C Programming is Awesome!
+
+### Learning more advanced topics of C programming language >>
+Function pointers in C
