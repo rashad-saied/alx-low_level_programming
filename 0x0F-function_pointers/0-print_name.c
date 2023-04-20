ede60139ec8ba827a88ddf6f40865f85eeac75c4
rashad7774 <rashadtech7@gmail.com>
2023-04-20 10:04:12 +0200

    nitialize a new project to manipulate function pointers in C

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,9 @@
  * print_name - prints a given name
  * @name: the given name
  * @f: a pointer to a function that takes a char pointer as an argument
- *
- * Return: void
  */
 
-void print_name(char *name, void (*f)(char *s))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 	{
