7121b3cd1006e36e69fda4c08d79054cb075179f
rashad7774 <rashadtech7@gmail.com>
2023-05-03 02:29:17 +0300

    Initialize new project to manipulate more singly linked lists in C.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
 /**
-
-
-
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: the head node of the list
+ *
+ * Return: the number of nodes
  */
 
 size_t print_listint(const listint_t *h)
